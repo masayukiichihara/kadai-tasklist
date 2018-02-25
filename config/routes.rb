0ff516c91ee3e4b9019171618e9d2b2fc8feb226
masayuki.ichihara <ec2-user@ip-172-31-84-236.ec2.internal>
2018-02-25 12:17:35 +0000

    top page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
+  get 'toppages/index'
+
   root to: 'tasks#index'
+  root to: 'toppages#index'
   
-  resources :tasks
+  get 'signup', to: 'users#new'
+  resources :users, only: [:index, :show, :new, :create]
 end
